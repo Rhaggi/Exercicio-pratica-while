c46e9490c6ab468c2200ec26bd7793604dc2e589
Rosa haggi <rosahaggi@gmail.com>
2025-07-31 12:41:48 -0300

    Criar IndicarQuadrante.java
    subindo código de exercício

---
+++ IndicarQuadrante.java
@@ -0,0 +1,31 @@
+/* Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema 
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo 
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma). */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int coordenadaX = sc.nextInt();
+        int coordenadaY = sc.nextInt();
+        
+        while(coordenadaX != 0 && coordenadaY != 0 ){
+            if (coordenadaX > 0 && coordenadaY > 0) {
+                System.out.println("1° quadrante");
+            } else if(coordenadaX < 0 && coordenadaY > 0) {
+                System.out.println("2° quadrante");
+            } else if (coordenadaX < 0 && coordenadaY < 0 ){
+                System.out.println("3° quadrante");
+            } else if(coordenadaX > 0 && coordenadaY < 0){
+                System.out.println("4° quadrante");
+            }
+            coordenadaX = sc.nextInt();
+            coordenadaY = sc.nextInt();
+        }
+        
+        sc.close();
+    }
+}
